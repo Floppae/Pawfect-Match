bffd991a27472619d11ee475d4f8e394a9c2e30c
Sifatun <sifanoor04@gmail.com>
2025-07-03 20:56:46 -0400

    added head section

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export default function Contact() {
+function Contact() {
   return (
-    <main>
-      <h1>Placeholder</h1>
+    <main className="min-h-screen px-4 bg-[#f5ebe0] font-playfair relative">
+      <section className="py-16 text-center px-4">
+        <h1 className="text-5xl font-bold text-neutral-900 mb-4">Contact Us</h1>
+        <p className="text-xl text-neutral-700">
+          We're always here to help you find your perfect pet companion.
+        </p>
+      </section>
     </main>
   );
 }
+
+export default Contact;
